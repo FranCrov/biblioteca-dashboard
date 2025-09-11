b4e383b6ba9d789ceaae2f035edc4f6cb02647dc
Franco Crovetto <francoignacio.crovetto@gmail.com>
2025-09-11 18:00:55 -0300

    Login, register and sign out

---
+++ prisma/migrations/20250911181205_add_user_table/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `usuario` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `usuario` DROP COLUMN `password`;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `user_username_key`(`username`),
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
